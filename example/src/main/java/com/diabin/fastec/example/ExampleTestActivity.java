2dafd89842ab2b9f521984c92b9465b6a5c92972
zhongchengt <zhongchengt@opera.com>
2018-12-07 11:14:16 +0800

    add files

--- example/src/main/java/com/diabin/fastec/example/ExampleTestActivity.java
+++ example/src/main/java/com/diabin/fastec/example/ExampleTestActivity.java
@@ -5,6 +5,7 @@ import com.diabin.latte.app.delegates.LatteDelegate;
 
 public class ExampleTestActivity  extends ProxyActivity{
     private static final int ONE = 1;
+    private static final int TWO = 2;
     @Override
     public LatteDelegate setRootDelegate() {
         return new ExampleDelegate();
