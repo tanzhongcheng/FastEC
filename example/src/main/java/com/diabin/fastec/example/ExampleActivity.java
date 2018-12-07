d65018495d921448b7c935fafc4775ed90173231
zhongchengt <zhongchengt@opera.com>
2018-12-07 11:15:11 +0800

    add THREE

--- example/src/main/java/com/diabin/fastec/example/ExampleActivity.java
+++ example/src/main/java/com/diabin/fastec/example/ExampleActivity.java
@@ -7,6 +7,7 @@ import com.diabin.latte.app.delegates.LatteDelegate;
 public class ExampleActivity extends ProxyActivity {
     private static final int ONE = 1;
     private static final int TWO = 2;
+    private static final int THREE = 3;
 
     @Override
     public LatteDelegate setRootDelegate() {
